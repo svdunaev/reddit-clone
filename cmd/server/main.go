8bee95e50d7688e2a93d9a2a37fc29aa47ba2e16
Stanislav <svdunaev95@gmail.com>
2026-05-15 07:58:05 +0500

    task/1-02: add logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"reddit-clone/internal/logger"
+
+	"github.com/joho/godotenv"
+)
+
+type App struct {
+	log *slog.Logger
+}
+
+func NewApp(log *slog.Logger) *App {
+	return &App{log: log}
+}
+
+func (a *App) Run() {
+	a.log.Info("Service running... ")
+}
 
 func main() {
-	fmt.Println("Hello!")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("No .env file, using system default values")
+	}
+
+	var level slog.Level
+
+	rawLogLevel := os.Getenv("LOG_LEVEL")
+	if err := level.UnmarshalText([]byte(rawLogLevel)); err != nil {
+		level = slog.LevelInfo
+	}
+
+	log := logger.New(level)
+
+	app := NewApp(log)
+
+	app.Run()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module reddit-clone
 
 go 1.25.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
