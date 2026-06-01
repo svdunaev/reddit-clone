03e15d2a8c2cab5241f91be1f6ab5163df4fdab4
Stanislav <svdunaev95@gmail.com>
2026-06-01 17:52:53 +0500

    task/1-05: write tests

--- internal/storage/inmem/inmem.go
+++ internal/storage/inmem/inmem.go
@@ -6,20 +6,22 @@ import (
 	"maps"
 	domain "reddit-clone/internal/domain/post"
 	"sync"
-	"time"
 
 	"github.com/google/uuid"
+	"k8s.io/utils/clock"
 )
 
 type store struct {
 	mu     sync.RWMutex
 	posts  map[uuid.UUID]*domain.Post
 	nextId int64
+	clock  clock.Clock
 }
 
-func New() *store {
+func New(clock clock.Clock) *store {
 	return &store{
 		posts: make(map[uuid.UUID]*domain.Post),
+		clock: clock,
 	}
 }
 
@@ -32,8 +34,8 @@ func (s *store) Create(ctx context.Context, input domain.Post) (domain.Post, err
 	}
 
 	input.Id = uuid.New()
-	input.CreatedAt = time.Now()
-	input.UpdatedAt = time.Now()
+	input.CreatedAt = s.clock.Now()
+	input.UpdatedAt = s.clock.Now()
 
 	s.posts[input.Id] = &input
 
@@ -48,7 +50,7 @@ func (s *store) List(ctx context.Context) ([]domain.Post, error) {
 		return []domain.Post{}, err
 	}
 
-	res := make([]domain.Post, len(s.posts))
+	res := make([]domain.Post, 0, len(s.posts))
 
 	for post := range maps.Values(s.posts) {
 		res = append(res, *post)
@@ -104,7 +106,9 @@ func (s *store) Update(ctx context.Context, id uuid.UUID, input domain.Post) (do
 		return domain.Post{}, fmt.Errorf("post with id %d does not exist", id)
 	}
 
+	input.Id = id
 	s.posts[id] = &input
+	s.posts[id].UpdatedAt = s.clock.Now()
 
 	return *s.posts[id], nil
 }
